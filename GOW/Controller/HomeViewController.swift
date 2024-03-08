8715b5cf47a4c49bbdcfd13324291de8f895c071
Andrés Martínez <arkantos890@gmail.com>
2024-03-08 17:21:06 -0600

    initial commit

---
+++ GOW/Controller/HomeViewController.swift
@@ -0,0 +1,38 @@
+//
+//  HomeViewController.swift
+//  GOW
+//
+//  Created by Developer on 08/03/24.
+//
+
+import UIKit
+
+class HomeViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
