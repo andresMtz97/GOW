090fc5a4707c1469c073bd88ddf41d4f4618d9d4
Il Garcia <il@MacBook-Air.local>
2024-03-10 12:45:08 -0600

    NavigationController and navigation bar's tint color added.

---
+++ GOW/Controller/NavigationViewController.swift
@@ -0,0 +1,28 @@
+//
+//  NavigationViewController.swift
+//  GOW
+//
+//  Created by Il Garcia on 10/03/24.
+//
+
+import UIKit
+
+class NavigationViewController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationBar.tintColor = UIColor(named: "GOWText")
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
